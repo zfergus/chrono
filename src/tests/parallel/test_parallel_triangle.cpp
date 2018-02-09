53a9a07ac9b664ff47a0f8c3c3a47219e5169db2
Radu Serban <serban@wisc.edu>
2018-02-09 13:42:40 +0100

    Use Chrono wrappers around OpenMP functions

--- src/tests/parallel/test_parallel_triangle.cpp
+++ src/tests/parallel/test_parallel_triangle.cpp
@@ -208,11 +208,11 @@ int main(int argc, char* argv[]) {
     // Set number of threads.
     // ----------------------
 
-    int max_threads = omp_get_num_procs();
+    int max_threads = CHOMPfunctions::GetNumProcs();
     if (threads > max_threads)
         threads = max_threads;
     msystem->SetParallelThreadNumber(threads);
-    omp_set_num_threads(threads);
+    CHOMPfunctions::SetNumThreads(threads);
     cout << "Using " << threads << " threads" << endl;
 
     // ---------------------
